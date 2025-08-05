c07c0b44fab8c262c9e7f8c62d41a6f1591147f5
WineIsFine90 <verest.bart@gmail.com>
2025-08-05 23:13:09 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import LePetitVin from './LePetitVin';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LePetitVin />
+  </React.StrictMode>
+);
