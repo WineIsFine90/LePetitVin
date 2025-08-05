357d2688acdfba06b1aa4f4613d05034f5524e51
WineIsFine90 <verest.bart@gmail.com>
2025-08-05 23:22:40 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import LePetitVin from './LePetitVin';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LePetitVin />
-  </React.StrictMode>
-);
